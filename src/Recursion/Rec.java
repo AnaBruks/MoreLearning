beea1a409e058bbdface4412b0549751002f94bc
AnaBruks <amanukhina@codempire.team>
2022-02-02 20:31:15 +0200

    2/02 commit with mistakes

--- src/Recursion/Rec.java
+++ src/Recursion/Rec.java
@@ -4,6 +4,8 @@ public class Rec {
     public static void main(String[] args) {
         int[] num = {1, 4,45,25,100, 25}; // sum=200
         System.out.println(recSum(num, 0));
+        System.out.println(count7(12_377_124_77)); //4
+
     }
 
     public static int recSum(int[] array, int i) {
@@ -11,7 +13,7 @@ public class Rec {
             return 0;
 
         return array[i] + recSum(array, i+1);
-        
+
 //        if (array.length == 0)
 //            return;
 //
@@ -21,5 +23,14 @@ public class Rec {
 //
 //        recSum(int [array.length - 1] array);
 
+    }
+    public static int count7 (long number){
+        if (number == 0) {
+            return 0;
+        }
+        else if (number % 10 == 7)
+            return 1 + count7(number / 10);
+        else
+            return count7(number/10);
     }
 }
